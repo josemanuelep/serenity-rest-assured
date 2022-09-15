ce9104ac27deabcd10a6287c0bab5f14c3fb6f43
Jose Manuel Echeverri <palacio.90@hotmail.com>
2022-09-15 10:25:22 -0500

    Test runner change

--- src/test/java/co/com/webtest/certification/globant/runners/TestRunner.java
+++ src/test/java/co/com/webtest/certification/globant/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.*;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "src/test/resources/features/",
-        tags = "@UpdateUser",
+//        tags = "@UpdateUser",
         glue = "co.com.webtest.certification.globant.stepsdefinitions"
 )
 public class TestRunner {
